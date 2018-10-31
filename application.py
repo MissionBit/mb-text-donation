b8aba35c0edb83e480d2889c9b647b602a335a84
juemura <ju.uemura@gmail.com>
2018-10-31 13:01:26 -0700

    Stripe checkout

--- application.py
+++ application.py
@@ -1,22 +1,44 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request
+import stripe
+
+stripe_keys = {
+  'secret_key': os.environ['SECRET_KEY'],
+  'publishable_key': os.environ['PUBLISHABLE_KEY']
+}
+
+
+stripe.api_key = stripe_keys['secret_key']
+
 app = Flask(__name__)
 
+@app.route('/<int:amount>/')
+def index(amount):
+    return render_template('index.html', key=stripe_keys['publishable_key'], amount=amount)
 
-@app.route("/")
-def index():
-    return "Index!"
- 
-@app.route("/hello")
-def hello():
-    return "Hello World!"
- 
-@app.route("/members")
-def members():
-    return "Members"
- 
-@app.route("/members/<string:name>/")
-def getMember(name):
-    return name
- 
-if __name__ == "__main__":
-    app.run()
+
+
+@app.route('/charge', methods=['POST'])
+def charge():
+    # Amount in cents
+    # amount = request.form['data-amount']
+    # description = request.form['data-amount']
+    amount = 500
+ 	description = "test"
+
+    customer = stripe.Customer.create(
+        email='customer@example.com',
+        source=request.form['stripeToken']
+    )
+
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        amount=amount,
+        currency='usd',
+        description=description
+    )
+
+    return render_template('charge.html', amount=amount)
+
+if __name__ == '__main__':
+    app.run(debug=True)
