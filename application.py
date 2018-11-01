49f9298b2609d82c70fafa0d092bc2aa76c8e037
Bob Ippolito <bob@redivi.com>
2018-11-01 15:46:17 -0700

    WIP

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import stripe
 
 stripe_keys = {
@@ -7,18 +7,29 @@ stripe_keys = {
   'publishable_key': os.environ['PUBLISHABLE_KEY']
 }
 
-
 stripe.api_key = stripe_keys['secret_key']
 
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
+
 @app.route('/')
 def redirect_to_give():
-    return redirect(url_for('.give', amount=2000), code=302)
+    return redirect(url_for('.give', dollars=20, _external=True), code=302)
 
-@app.route('/<int:amount>/')
-def give(amount):
-    return render_template('index.html', key=stripe_keys['publishable_key'], amount=amount)
+@app.route('/<int:dollars>/')
+def give(dollars):
+    return render_template(
+        'index.html',
+        key=stripe_keys['publishable_key'],
+        amount=dollars * 100
+    )
 
 @app.route('/charge', methods=['POST'])
 def charge():
