ddc4458b0db6c53b61a382be1f0c53a9dbd5f509
juemura <ju.uemura@gmail.com>
2018-11-01 17:08:01 -0700

    Redirected 404 to amount entry page

--- application.py
+++ application.py
@@ -16,9 +16,15 @@ app = Flask(__name__)
 def index(message):
     return render_template('index.html', key=stripe_keys['publishable_key'], amount=100*message)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('default.html', key=stripe_keys['publishable_key']), 200
+
 @app.route('/charge', methods=['POST'])
 def charge():
-    amount = request.form.get('amount', type=int)
+    amount = request.form.get('amount', type=float)
+    amount = int(amount * 100);
+
 
     customer = stripe.Customer.create(
         email=request.form['stripeEmail'],
